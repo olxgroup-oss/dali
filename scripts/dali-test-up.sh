30529cac0d37c3c42c7a499e1d0daf452adcc11c
Gabriel Melillo <gabriel@melillo.me>
2022-03-03 14:38:05 +0100

    ci(test): propagate error to make
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- scripts/dali-test-up.sh
+++ scripts/dali-test-up.sh
@@ -4,6 +4,8 @@ cargo build
 PID=$!
 docker run --rm -v $(pwd)/tests/resources/:/usr/share/nginx/html/ -p 9000:80 --name dali-http-source -d nginx
 cargo test
+RCODE=$?
 docker stop dali-http-source
 kill $PID
 wait $PID
+exit $RCODE
