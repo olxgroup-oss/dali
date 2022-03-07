e770d361ebd1f2e786ba177fab9a9b1717246014
Gabriel Melillo <gabriel@melillo.me>
2022-03-07 16:19:46 +0100

    refactor(tests): update test script to make it more readable
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- scripts/dali-test-up.sh
+++ scripts/dali-test-up.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-cargo build --no-default-features --features "hyper_client"
-./target/debug/dali >> /dev/null &
-PID=$!
-docker run --rm -v $(pwd)/tests/resources/:/usr/share/nginx/html/ -p 9000:80 --name dali-http-source -d nginx
-cargo test --no-default-features --features "hyper_client"
+
+run_test_with_feature() {
+    cargo build --no-default-features --features "${1}"
+    ./target/debug/dali >> /dev/null &
+    PID=$!
+    cargo test --no-default-features --features "${1}"
+    RCODE=$?
+    stop_process ${PID}
+}
+
+stop_process() {
+    kill ${1}
+    wait ${1}
+}
+
+setup() {
+    docker run --rm -v $(pwd)/tests/resources/:/usr/share/nginx/html/ -p 9000:80 --name dali-http-source -d nginx
+}
+
+teardown() {
+    docker stop dali-http-source
+}
+
+setup
+
+run_test_with_feature "hyper_client"
 RCODE=$?
-kill ${PID}
-wait ${PID}
-cargo build --no-default-features --features "awc_client"
-./target/debug/dali >> /dev/null &
-PID=$!
-cargo test --no-default-features --features "awc_client"
+
+run_test_with_feature "awc_client"
 AWCRCODE=$?
-docker stop dali-http-source
-kill ${PID}
-wait ${PID}
+
+teardown
+
 if [[ ${RCODE} -ne 0 || ${AWCRCODE} -ne 0 ]]; then
     exit 1
 fi
