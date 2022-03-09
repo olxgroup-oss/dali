4cb890105db9ce1edfb700aeb47d1836b4c67147
Gabriel Melillo <gabriel@melillo.me>
2022-03-09 18:33:39 +0100

    tests: updated some default and wait for ports
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- scripts/dali-test-up.sh
+++ scripts/dali-test-up.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 run_test_with_feature() {
-    cargo build --no-default-features --features "${1}"
-    ./target/debug/dali >> /dev/null &
+    cargo run --no-default-features --features "${1}" >> /dev/null &
     PID=$!
+    wait_until_ready localhost 8080
     cargo test --no-default-features --features "${1}"
     RCODE=$?
-    stop_process ${PID}
+    stop_process ${PID} localhost 8080
+}
+
+wait_until_ready() {
+    timeout 30 sh -c 'until nc -z $0 $1; do sleep 1; done' $1 $2
+}
+
+wait_until_notready() {
+    timeout 30 sh -c 'until ! nc -z $0 $1; do sleep 1; done' $2 $3
 }
 
 stop_process() {
-    kill ${1}
-    wait ${1}
+    kill -SIGTERM ${1}
+    wait_until_notready $2 $3
 }
 
 setup() {
     docker run --rm -v $(pwd)/tests/resources/:/usr/share/nginx/html/ -p 9000:80 --name dali-http-source -d nginx
-    timeout 30 sh -c 'until nc -z $0 $1; do sleep 1; done' localhost 9000
+    wait_until_ready localhost 9000
 }
 
 teardown() {
