fcfdfd6471f40417b8654637227f6c41f1917e41
bogdan.vidrean <bogdan.vidrean@olx.com>
2026-05-27 16:20:19 +0300

    chore(deps): adhere to the new api of libvips

--- src/image_processor/mod.rs
+++ src/image_processor/mod.rs
@@ -108,7 +108,7 @@ pub fn process_image(
             wm
         };
         final_image =
-            ops::composite_2_with_opts(&final_image, &wm, ops::BlendMode::Over, &options)?;
+            ops::composite2_with_opts(&final_image, &wm, ops::BlendMode::Over, &options)?;
     }
 
     debug!("Encoding to: {}", format);
